16f129a7cbd039de74e9a3399d8b029ab79fcd97
tsmolinski <tsmolinski3@gmail.com>
2024-12-20 12:39:50 +0100

    Another changes due to UE 5.4.4 version.

--- Source/cupcake.Target.cs
+++ Source/cupcake.Target.cs
@@ -8,7 +8,7 @@ public class cupcakeTarget : TargetRules
 	public cupcakeTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "cupcake" } );
 	}
--- Source/cupcakeEditor.Target.cs
+++ Source/cupcakeEditor.Target.cs
@@ -8,7 +8,7 @@ public class cupcakeEditorTarget : TargetRules
 	public cupcakeEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
 
 		ExtraModuleNames.AddRange( new string[] { "cupcake" } );
 	}
